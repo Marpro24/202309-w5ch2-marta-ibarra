78873e6dca457a6d5616b6242f725fd85191ecf6
MarIb <martaa.ibarra@hotmail.com>
2023-10-10 23:36:50 +0200

    Add third and fourth testing cases

--- app/strictEquals/strictEquals.test.js
+++ app/strictEquals/strictEquals.test.js
@@ -24,4 +24,28 @@ describe("Given a strictEqual function", () => {
       expect(hasEqualValue).toBe(result);
     });
   });
+
+  describe("When it receives 0 and -0", () => {
+    test("Then it should return true", () => {
+      const valueA = 0;
+      const valueB = -0;
+      const result = true;
+
+      const hasEqualValue = strictEquals(!valueA, !valueB);
+
+      expect(hasEqualValue).toBe(result);
+    });
+  });
+
+  describe("When it receives 0 and -0", () => {
+    test("Then it should return true", () => {
+      const valueA = -0;
+      const valueB = 0;
+      const result = true;
+
+      const hasEqualValue = strictEquals(!valueA, !valueB);
+
+      expect(hasEqualValue).toBe(result);
+    });
+  });
 });
